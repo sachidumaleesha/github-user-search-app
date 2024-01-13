c252a3c9fab4c538a2c3949eed9b89607b4c3a5d
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-01-14 00:51:19 +0530

    remove border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen w-full bg-slate-100 p-1.5 sm:p-4 pt-10 sm:pt-12 transition dark:bg-slate-900">
-      <div className="mx-auto flex w-full max-w-[600px] flex-col gap-8 rounded p-2 border dark:border-none">
+      <div className="mx-auto flex w-full max-w-[600px] flex-col gap-8 rounded p-2">
         <section className="flex justify-between gap-3">
           <p className="text-xl font-semibold">devfinder</p>
           <DarkandLightButton />
